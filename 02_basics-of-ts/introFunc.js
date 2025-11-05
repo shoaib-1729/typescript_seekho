1829ac3b363ee84cfebed5bc5c0037988de293a2
shoaib-1729 <shoaibahktar56@gmail.com>
2025-11-05 20:29:24 +0530

    handling functions in typescript

--- 02_basics-of-ts/introFunc.js
+++ 02_basics-of-ts/introFunc.js
@@ -25,3 +25,60 @@ var signinUser = function (name, email, isUserVerified) {
     return isSigninUser;
 };
 signinUser("Raghav", "raghav@store.com");
+// functions with return type
+// what if no return is specified ?
+function doubleNum(num) {
+    // this could return a string which is wrong here, since this should return a number
+    return " ";
+    // return num * 2
+}
+var val = doubleNum(8);
+console.log(val);
+// correct syntax
+function tripleNum(num) {
+    return num * 3;
+}
+console.log(tripleNum(5));
+// arrow function syntax
+var isEven = function (num) {
+    if (num % 2 == 0) {
+        return true;
+    }
+    return false;
+};
+console.log(isEven(9));
+// ....
+// special scenerios while handling functions in typescript
+// case of no return
+// typescript is smart enough to detect what is being returned, so is these cases we can reply on typescript 
+function sayHello() {
+    console.log("Hello, Welcome Home...");
+}
+sayHello();
+// when multiple types of values are being returned - use union types
+function welcomeUserMsg(gender) {
+    if (gender === "Male") {
+        return 'Namaste';
+    }
+    else if (gender === "Female") {
+        return 1;
+    }
+    else {
+        return true;
+    }
+}
+var val1 = welcomeUserMsg("Male");
+var val2 = welcomeUserMsg("Female");
+var val3 = welcomeUserMsg("Others");
+console.log(val1, val2, val3);
+// case of native functions 
+// map
+var items = ["Aloo", "Matar", "Tamatar", "Pepsi"];
+var mappedItems = items.map(function (item) { return ({
+    billedItems: item
+}); });
+console.log(mappedItems);
+var filteredItems = items.filter(function (item) {
+    return item === "Aloo" || item === "Matar" || item === "Tamatar" ? true : false;
+});
+console.log(filteredItems);
--- 02_basics-of-ts/introFunc.ts
+++ 02_basics-of-ts/introFunc.ts
@@ -35,4 +35,91 @@ const signinUser = (name: string, email: string, isUserVerified: boolean = true)
 
 signinUser("Raghav", "raghav@store.com")
 
+// functions with return type
+
+// what if no return is specified ?
+
+function doubleNum(num: number){
+
+    // this could return a string which is wrong here, since this should return a number
+    return " "
+
+    // return num * 2
+}
+
+const val = doubleNum(8)
+console.log(val);
+
+
+// correct syntax
+function tripleNum(num: number): number{
+    return num * 3
+}
+
+console.log(tripleNum(5))
+
+// arrow function syntax
+const isEven = (num: number): boolean => {
+    if(num%2==0){
+        return true
+    }
+
+    return false
+}
+
+console.log(isEven(9))
+
+// ....
+
+// special scenerios while handling functions in typescript
+// case of no return
+
+// typescript is smart enough to detect what is being returned, so is these cases we can reply on typescript 
+function sayHello(): void{
+    console.log("Hello, Welcome Home...")
+}
+
+sayHello()
+
+// when multiple types of values are being returned - use union types
+function welcomeUserMsg(gender: string): string | number | boolean{
+    if(gender === "Male"){
+        return 'Namaste'
+    }
+
+    else if(gender === "Female"){
+        return 1
+    }
+
+    else{
+        return true
+    }
+}
+
+const val1 = welcomeUserMsg("Male")
+const val2 = welcomeUserMsg("Female")
+const val3 = welcomeUserMsg("Others")
+
+console.log(val1, val2, val3)
+
+// case of native functions 
+
+// map
+const items = ["Aloo", "Matar", "Tamatar", "Pepsi"]
+
+const mappedItems = items.map((item)=> ({
+    billedItems: item
+}))
+
+console.log(mappedItems)
+
+const filteredItems = items.filter((item)=> {
+    return item === "Aloo" || item === "Matar" || item === "Tamatar" ? true : false;
+})
+
+console.log(filteredItems)
+
+
+
+
 export {}
