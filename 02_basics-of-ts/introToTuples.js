7acafe0737b19438fd9834a763b44100c1928ad0
shoaib-1729 <shoaibahktar56@gmail.com>
2025-11-11 18:39:43 +0530

    working on tuples in ts

---
+++ 02_basics-of-ts/introToTuples.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var userIds = [828, "717", "919"];
+// array of fix length
+var ids = ["0", 282, 919];
+// use cases
+var rgb = [171, 118, 219];
+// since, tuple bss javascript ke array ke upar ek extra type layer add karta hai, isiliye yeh array ke saare operations ko support karta hai, aur yahi iska major drawback bhi hai ki tuple conceptual level par fixed length ka hone ka baawjood bhi technically grow kar sakta hai
+rgb.push(818);
+console.log(rgb);
+// solutions
+// make it readonly taaki push naa ho paaye
+var ratings = [74, "Good", 64];
+// now this is not allowed
+// ratings.push("Bad")
+console.log(ratings);
+// use cases
+// tuples with functions
+function greetUser(_a) {
+    var greeting = _a.greeting, userId = _a.userId;
+    return [greeting, userId];
+}
+var res = greetUser({
+    greeting: "Hello",
+    userId: 123
+});
+var name = res[0], id = res[1];
+console.log(res);
+console.log(res.join(""));
---
+++ 02_basics-of-ts/introToTuples.ts
@@ -0,0 +1,40 @@
+let userIds: (string | number)[] = [828, "717", "919"]
+
+// array of fix length
+let ids: [string, number, number] = ["0", 282, 919]
+
+// use cases
+const rgb: [number, number, number] = [171, 118, 219]
+
+// since, tuple bss javascript ke array ke upar ek extra type layer add karta hai, isiliye yeh array ke saare operations ko support karta hai, aur yahi iska major drawback bhi hai ki tuple conceptual level par fixed length ka hone ka baawjood bhi technically grow kar sakta hai
+rgb.push(818)
+
+console.log(rgb)
+
+// solutions
+// make it readonly taaki push naa ho paaye
+let ratings:readonly [number, string, number] = [74, "Good", 64]
+
+// now this is not allowed
+// ratings.push("Bad")
+
+console.log(ratings);
+
+// use cases
+// tuples with functions
+function greetUser({greeting, userId}: {greeting: string, userId: number}): [string,number]{
+    return [greeting, userId]
+}
+
+const res  = greetUser({
+    greeting:"Hello",
+    userId:123
+})
+let [name, id] = res;
+console.log(res);
+console.log(res.join(""));
+
+
+
+
+export {}
